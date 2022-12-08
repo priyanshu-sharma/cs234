ab131032e8062c26822b33b225cc3cbb45ce3307
Priyanshu Sharma <prynsh1112@gmail.com>
2022-12-08 13:04:31 -0800

    added basic construction implementation

--- ukkonen.py
+++ ukkonen.py
@@ -2,6 +2,18 @@ import time
 import logging
 logging.basicConfig(level=logging.INFO)
 
+def construct_suffix_tree_using_ukkonen(formatted_string):
+    '''
+    Responsiblity: Main Method to build Ukkonen
+        Based Suffix Tree
+    Input: formatted_string = input_string + "$"
+    Ouput: Newly Contructed Suffix Tree Based on
+        Ukkonen Algorithm
+    '''
+    # ukkonen_suffix_tree = UkkonenSuffixTree(formatted_string)
+    # ukkonen_suffix_tree.construct_suffix_tree()
+    return 1
+
 def user_input_flow():
     '''
     Responsiblity: Mainly involved in taking
@@ -20,7 +32,8 @@ def user_input_flow():
     logging.info("Your fomatted string will be : {}\n".format(formatted_string))
     logging.info("----------Started Constructing Suffix Tree-------------")
     start_time = time.time()
-    # ukkonen_suffix_tree = construct_suffix_tree_using_ukkonen(formatted_string)
+    ukkonen_suffix_tree = construct_suffix_tree_using_ukkonen(formatted_string)
+    print(ukkonen_suffix_tree)
     end_time = time.time()
     logging.info("----------Suffix Tree Construction Completed-------------")
     logging.info("Construction of Suffix Tree using Ukkonen Algorithm took around : {} seconds".format(end_time - start_time))
